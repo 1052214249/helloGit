7f8be41b9ea3690a2ccb397122981e8be8e227c6
lkh <1052214249@qq.com>
2021-01-23 21:02:03 +0800

    HelloGit v0.1

---
+++ HelloGit/src/main/java/com/lkh/git/HelloGit.java
@@ -0,0 +1,11 @@
+package com.lkh.git;
+
+/**
+ * @author lkh
+ * @create 2021-01-23-20:45
+ */
+public class HelloGit {
+    public static void main(String[] args) {
+        System.out.println("Hello Git!");
+    }
+}
